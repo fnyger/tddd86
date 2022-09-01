7ffad9bfea69aeff395c5882a49f578594ffac16
Filip Nygren <filny841@student.liu.se>
2022-09-01 14:39:52 +0200

    createGrid and printGrid

--- life/src/life.cpp
+++ life/src/life.cpp
@@ -6,10 +6,57 @@
 #include "grid.h"
 #include "lifeutil.h"
 #include <string>
+#include <fstream>
+using namespace std;
+
+Grid<string> createGrid();
+void printGrid(const Grid<string>& grid);
+
 int main() {
 
     // TODO: Finish the program!
 
-    std::cout << "Have a nice Life! "  << std::endl;
+    Grid<string> grid = createGrid();
+
+    printGrid(grid);
+
     return 0;
+}
+
+
+
+void printGrid(const Grid<string>& grid) {
+    int width = grid.numCols();
+    int height = grid.numRows();
+
+    for (int y = 0; y<height; y++) {
+        for (int x = 0; x<width; x++) {
+            cout << grid[y][x];
+        }
+        cout << endl;
+    }
+}
+
+
+Grid<string> createGrid() {
+
+    std::ifstream input;
+    input.open("../mycolony.txt");
+    string line;
+
+    getline(input, line);
+    int height = stoi(line);
+    getline(input, line);
+    int width = stoi(line);
+    Grid<string> grid(height, width);
+
+    getline(input, line); //clear line
+
+    for(int y=0; y<height; y++) {
+        getline(input, line);
+        for (int x=0; x<width; x++) {
+            grid[y][x] = line.at(x);
+        }
+    }
+    return grid;
 }
