3b366d526447cacdc303b86ccd3953c9c825cdb3
filny841 <filny841@student.liu.se>
2022-09-06 11:58:48 +0200

    read dict

--- lab2/wordchain/src/wordchain.cpp
+++ lab2/wordchain/src/wordchain.cpp
@@ -3,9 +3,13 @@
 #include <string>
 #include <queue>
 #include <stack>
+#include <map>
 using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
+
+map<string, bool> createDict();
+
 
 int main() {
     cout << "Welcome to TDDD86 Word Chain." << endl;
@@ -13,9 +17,27 @@ int main() {
     cout << "first into the second by changing one letter at a time." << endl;
     cout << endl;
 
-    cout << "Please type two words: ";
+    cout << "Please type two words: " << endl;
+
+    queue<stack<string>> stacks;
+
+    map<string, bool> dict = createDict();
+
+    cout << dict["zebsdfgsdfgra"] << endl;
 
     // TODO: Finish the program!
 
     return 0;
+}
+
+map<string, bool> createDict() {
+
+    ifstream input;
+    input.open("dictionary.txt");
+    string line;
+    map<string, bool> dict;
+    while (getline(input, line)) {
+        dict[line] = true;
+    }
+    return dict;
 }
