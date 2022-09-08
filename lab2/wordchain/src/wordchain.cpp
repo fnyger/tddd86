e68e1e8c5828a9ec1a7ce367a3cbdde3ecf01b71
Filip Nygren <filny841@student.liu.se>
2022-09-08 13:29:49 +0200

    lab2 wordchain

--- lab2/wordchain/src/wordchain.cpp
+++ lab2/wordchain/src/wordchain.cpp
@@ -4,11 +4,17 @@
 #include <queue>
 #include <stack>
 #include <map>
+#include <unordered_set>
 using namespace std;
 
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
-map<string, bool> createDict();
+unordered_set<string> createDict();
+stack<string> wordChain(string w1, string w2);
+bool validWord(string word, const unordered_set<string>& dictionary);
+bool used(string newWord, const unordered_set<string>& usedWords);
+
+
 
 
 int main() {
@@ -17,27 +23,80 @@ int main() {
     cout << "first into the second by changing one letter at a time." << endl;
     cout << endl;
 
-    cout << "Please type two words: " << endl;
+    cout << "Please type two words: ";
+    string w1, w2;
+    cin >> w1;
+    cin >> w2;
 
-    queue<stack<string>> stacks;
+    cout << "Chain from data back to code:" << endl;
 
-    map<string, bool> dict = createDict();
+    stack<string> chain = wordChain(w1, w2);
 
-    cout << dict["zebsdfgsdfgra"] << endl;
+    do {
+        cout << chain.top() << " ";
+        chain.pop();
+    } while(!chain.empty());
 
-    // TODO: Finish the program!
+
+    cout << endl << "Have a nice day!" << endl;
 
     return 0;
 }
 
-map<string, bool> createDict() {
+stack<string> wordChain(string w1, string w2) {
+
+    stack<string> startStack;
+    startStack.push(w1);
+    queue<stack<string>> stacks;
+    stacks.push(startStack);
+
+    unordered_set<string> dictionary = createDict();
+    unordered_set<string> usedWords;
+
+
+    while(!stacks.empty()) {
+        stack<string> workStack = stacks.front();
+        stacks.pop();
+        if(workStack.top() == w2) {
+            return workStack;
+        } else {
+            string currentWord = workStack.top();
+            for(int i = 0; i < currentWord.length(); i++) {
+                for(const char& c : ALPHABET) {
+                    string newWord = currentWord;
+                    newWord[i] = c;
+                    if(validWord(newWord, dictionary)) {
+
+                        if(!used(newWord, usedWords)) {
+                            usedWords.insert(newWord);
+                            stack<string> newStack (workStack);
+                            newStack.push(newWord);
+                            stacks.push(newStack);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool validWord(string word, const unordered_set<string>& dictionary) {
+    return dictionary.count(word) >= 1;
+
+}
+
+bool used(string newWord, const unordered_set<string>& usedWords) {
+    return usedWords.count(newWord) >= 1;
+}
+
+unordered_set<string> createDict() {
 
     ifstream input;
     input.open("dictionary.txt");
     string line;
-    map<string, bool> dict;
+    unordered_set<string> dict;
     while (getline(input, line)) {
-        dict[line] = true;
+        dict.insert(line);
     }
     return dict;
 }
