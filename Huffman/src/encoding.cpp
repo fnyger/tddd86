672e5c6107a8b2dde10008eee117ed8f79ac305b
felra653 <felra653@student.liu.se>
2022-11-23 16:55:12 +0100

    Påbörjat extra lab 1

--- Huffman/src/encoding.cpp
+++ Huffman/src/encoding.cpp
@@ -4,17 +4,45 @@
 // TODO: remove this comment header
 
 #include "encoding.h"
+#include "HuffmanNode.h"
+#include <queue>
+using namespace std;
 // TODO: include any other headers you need
 
 map<int, int> buildFrequencyTable(istream& input) {
-    // TODO: implement this function
     map<int, int> freqTable;
+    while(input) {
+        char newChar = input.get();
+        if (newChar == -1) break;
+        if (freqTable.count(newChar)) {
+            freqTable[newChar] += 1;
+        } else {
+            freqTable.emplace(newChar, 1);
+        }
+    }
+    freqTable.emplace(PSEUDO_EOF, 1);
     return freqTable;
 }
 
 HuffmanNode* buildEncodingTree(const map<int, int> &freqTable) {
-    // TODO: implement this function
-    return nullptr;
+    priority_queue<HuffmanNode> huffManQueue;
+    for (auto it = freqTable.begin(); it != freqTable.end();it++){
+            HuffmanNode node = HuffmanNode(it->first,it->second);
+            huffManQueue.push(node);
+        }
+    HuffmanNode parent;
+    while(huffManQueue.size() > 1) {
+        HuffmanNode* firstChild = new HuffmanNode(huffManQueue.top().character, huffManQueue.top().count,//
+                                                  huffManQueue.top().zero, huffManQueue.top().one);
+        huffManQueue.pop();
+        HuffmanNode* secondChild = new HuffmanNode(huffManQueue.top().character, huffManQueue.top().count,//
+                                                  huffManQueue.top().zero, huffManQueue.top().one);
+        huffManQueue.pop();
+        parent = HuffmanNode(NOT_A_CHAR, firstChild->count+secondChild->count, firstChild, secondChild);
+        huffManQueue.push(parent);
+    }
+    return new HuffmanNode(huffManQueue.top().character, huffManQueue.top().count,//
+                            huffManQueue.top().zero, huffManQueue.top().one);
 }
 
 map<int, string> buildEncodingMap(HuffmanNode* encodingTree) {
