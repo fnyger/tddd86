e170d59ff36b6410849dd340f9c3d94f076c5bfb
Filip Nygren <filny841@student.liu.se>
2022-12-02 14:12:40 +0100

    prioqueue

--- labb6/MyPriorityQueue.h
+++ labb6/MyPriorityQueue.h
@@ -33,49 +33,128 @@ public:
   unsigned size() const;
 
 private:
-    // Other private members?
+
+  unsigned parentIndex(unsigned index);
+
+  int leftChildIndex(unsigned index);
+
+  int rightChildIndex(unsigned index);
+
+  void swap(unsigned index1, unsigned index2);
+
 };
+
+
 
 template <typename T, typename C>
 MyPriorityQueue<T,C>::MyPriorityQueue(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
 }
 
 template <typename T, typename C>
 MyPriorityQueue<T,C>::~MyPriorityQueue(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+
 }
 
 template <typename T, typename C>
 void MyPriorityQueue<T,C>::push(const T& t){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    vector_array.push_back(t);
+
+    unsigned index = vector_array.size() - 1;
+    unsigned parent_index = parentIndex(index);
+
+    while(index > 0 && strictly_larger_operator(vector_array[parent_index], vector_array[index])) {
+        swap(index, parent_index);
+        index = parent_index;
+        parent_index = parentIndex(index);
+    }
 }
 
 template <typename T, typename C>
 T MyPriorityQueue<T,C>::top()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+
+    return vector_array[0];
 }
 
 template <typename T, typename C>
 void MyPriorityQueue<T,C>::pop(){
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    vector_array[0] = vector_array[vector_array.size()-1];
+    vector_array.pop_back();
+    int index = 0;
+    while(index != -1) {
+        int leftChild = leftChildIndex(index);
+        int rightChild = rightChildIndex(index);
+
+        //No children
+        if(leftChild == -1) {
+            index = leftChild;
+        } else if(rightChild == -1) { //only left children
+
+            if(strictly_larger_operator(vector_array[index], vector_array[leftChild])) {
+                swap(leftChild, index);
+                index = leftChild;
+            }
+            index = -1;
+
+        } else { //Two children
+            int largestChildIndex;
+            if(strictly_larger_operator(vector_array[rightChild], vector_array[leftChild])) {
+                largestChildIndex = leftChild;
+            } else {
+                largestChildIndex = rightChild;
+            }
+
+            if(strictly_larger_operator(vector_array[index], vector_array[largestChildIndex])) {
+                swap(largestChildIndex, index);
+                index = largestChildIndex;
+            } else {
+                index = -1;
+            }
+
+        }
+    }
 }
 
 template <typename T, typename C>
 bool MyPriorityQueue<T,C>::empty()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+    return vector_array.size() == 0;
+
 }
 
 template <typename T, typename C>
 unsigned MyPriorityQueue<T,C>::size()const{
-    // TODO: replace the code below with your code for this member
-    MYEXCEPTION("unimplemented method");
+
+    return vector_array.size();
+}
+
+template <typename T, typename C>
+unsigned MyPriorityQueue<T,C>::parentIndex(unsigned index){
+
+    return index ? (int)((index - 1) / 2) : 0;
+}
+
+template <typename T, typename C>
+int MyPriorityQueue<T,C>::leftChildIndex(unsigned index){
+    unsigned newIndex = 2*index+1;
+    if(newIndex < size()) {
+        return newIndex;
+    }
+    return -1;
+}
+
+template <typename T, typename C>
+int MyPriorityQueue<T,C>::rightChildIndex(unsigned index){
+    unsigned newIndex = 2*index+2;
+    if(newIndex < size()) {
+        return newIndex;
+    }
+    return -1;
+}
+
+template <typename T, typename C>
+void MyPriorityQueue<T,C>::swap(unsigned index1, unsigned index2){
+    T temp = vector_array[index1];
+    vector_array[index1] = vector_array[index2];
+    vector_array[index2] = temp;
 }
 
 #endif // MY_PRIORITY_QUEUE_H
